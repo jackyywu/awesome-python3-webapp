e7fbd4097428f203799879c933f8a436e68a1ab6
WSX\wsx <420476948@qq.com>
2020-07-03 17:21:58 +0800

    add coroweb.py

--- www/coroweb.py
+++ www/coroweb.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import asynico,os,inspect,logging,functools
+import asyncio, os, inspect, logging, functools
+
 from urllib import parse
-from aiohtto import web
+
+from aiohttp import web
+
+from apis import APIError
 
 
 def get(path):
 	#定义get方法
 	def decorator(func):
 		@functools.wraps(func)
-		def wrapper(**args,**kw):
-			return func(**args,**kw)
+		def wrapper(*args,**kw):
+			return func(*args,**kw)
 		wrapper.__method__ = 'GET'
 		wrapper.__route__ = path
 		return wrapper
@@ -21,52 +25,53 @@ def post(path):
 	#定义post方法
 	def decorator(func):
 		@functools.wraps(func)
-		def wrapper(**args,**kw):
-			return func(**args,**kw)
+		def wrapper(*args,**kw):
+			return func(*args,**kw)
 		wrapper.__method__ = 'POST'
 		wrapper.__route__ = path
 		return wrapper
 	return decorator
 
 def get_required_kw_args(fn):
-	args=[]
-	params=inspect.signature(fn).parameters
-	for name,param in param.items():
-		if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
-			args.append(name)
-		return tuple(args)
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
 
 def get_named_kw_args(fn):
-	args=[]
-	params = inspect.signature(fn).parameters
-	for name,param in param.items():
-		if param.kind == inspect.Parameter.KEYWORD_ONLY:
-			args.append(name)
-		return tuple(args)
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            args.append(name)
+    return tuple(args)
 
 def has_named_kw_args(fn):
-	params = inspect.signature(fn).parameters
-	for name,param in param.items():
-		if param.kind == inspect.Parameter.KEYWORD_ONLY:
-			return True
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            return True
 
 def has_var_kw_arg(fn):
-	params = inspect.signature(fn).parameters
-	for name,param in param.items():
-		if param.kind == inspect.Parameter.VAR_KEYWORD:
-			return True
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.VAR_KEYWORD:
+            return True
 
 def has_request_arg(fn):
-	sig = inspect.signature(fn)
-	params = sig.parameters
-	found = False
-	for name,param in params.items():
-		if name == 'request':
-			found = True
-			continue
-		if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
-			raise ValueError('request Parameter must be the last named parameter in function:%s%s' % (fn.__name__,str(sig)))
-
+    sig = inspect.signature(fn)
+    params = sig.parameters
+    found = False
+    for name, param in params.items():
+        if name == 'request':
+            found = True
+            continue
+        if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
+            raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
+    return found
+    
 class RequestHandler(object):
 	def __init__(self,app,fn):
 		self._app = app
@@ -127,21 +132,21 @@ def add_static(app):
     logging.info('add static %s => %s' % ('/static/', path))
 
 def add_route(app,fn):
-	method = getattr(fn,'__method__',None):
-	path = getattr(fn,'__route__',None):
+	method = getattr(fn,'__method__',None)
+	path = getattr(fn,'__route__',None)
 	if path is None or method is None:
 		raise ValueError('@get or @post not defined in %s.' % str(fn))
-	if not asynico.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
-		fn = asynico.coroutine(fn)
+	if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+		fn = asyncio.coroutine(fn)
 	logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
 	app.router.add_route(method,path,RequestHandler(app,fn))
 
-def add_routes(app,model_name):
-	n = model_name.rfind('.')
+def add_routes(app,module_name):
+	n = module_name.rfind('.')
 	if n ==(-1):
 		mod = __import__(module_name,globals(),locals())
 	else:
-		name = model_name[n+1:]
+		name = module_name[n+1:]
 		mod = getattr(__import__(module_name[:n],globals(),[name]),name)
 	for attr in dir(mod):
 		if attr.startswith('_'):
