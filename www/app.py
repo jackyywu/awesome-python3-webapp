572901d250c7d08a95e6faedb75d80067076a14c
WSX\wsx <420476948@qq.com>
2020-07-09 17:23:31 +0800

    add app.py

--- www/app.py
+++ www/app.py
@@ -9,8 +9,12 @@ from datetime import datetime
 from aiohttp import web
 from jinja2 import Environment, FileSystemLoader
 
+from config import configs
+
 import orm
 from coroweb import add_routes,add_static
+
+from handler import cookie2user, COOKIE_NAME
 
 def init_jinja2(app, **kw):
 	logging.info('init jinja2...')
@@ -39,6 +43,22 @@ async def logger_factory(app, handler):
 		await asyncio.sleep(0.3)
 		return (await handler(request))
 	return logger
+
+async def auth_factory(app, handler):
+    async def auth(request):
+        logging.info('check user: %s %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = await cookie2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.email)
+                request.__user__ = user
+        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+            return web.HTTPFound('/signin')
+        return (await handler(request))
+    return auth
+
 async def data_factory(app, handler):
 	async def parse_data(request):
 		if request.method == 'POST':
@@ -74,6 +94,7 @@ async def response_factory(app, handler):
 				resp.content_type = 'application/json;charset=utf-8'
 				return resp
 			else:
+				r['__user__'] = request.__user__
 				resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
 				resp.content_type = 'text/html;charset=utf-8'
 				return resp
@@ -101,9 +122,6 @@ def datetime_filter(t):
 		return u'%s天前' % (delta // 86400)
 	dt = datetime.fromtimestamp(t)
 	return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
-
-def index(request):
-	return web.Response(body=b'<h1>Awesome<h1>',content_type='text/html')
 	
 async def init(loop):
 	await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='password', db='awesome')
@@ -111,12 +129,13 @@ async def init(loop):
 		logger_factory,response_factory
 	])
 	init_jinja2(app,filters=dict(datetime=datetime_filter))
-	add_routes(app,'handlers')
+	add_routes(app,'handler')
 	add_static(app)
 	srv= await loop.create_server(app.make_handler(),'127.0.0.1',9000)
 	logging.info('server started at http://127.0.0.1:9000')
 	return srv
 	
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
