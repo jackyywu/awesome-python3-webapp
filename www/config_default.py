527f8409bee80d906aad0757d47af825f30abd3f
WSX\wsx <420476948@qq.com>
2020-07-09 17:23:59 +0800

    add config_default.py

--- www/config_default.py
+++ www/config_default.py
@@ -1,12 +1,13 @@
 # config_default.py
 
 configs = {
+	'debug': True,
 	'db' :{
 		'host':'127.0.0.1',
 		'port':3306,
 		'user':'root',
 		'password':'password',
-		'database':'awesome'	
+		'db':'awesome'	
 	},
 	'session':{
 			'secret':'Awesome'
